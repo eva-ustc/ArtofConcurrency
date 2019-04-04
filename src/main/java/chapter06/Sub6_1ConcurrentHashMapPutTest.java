ce0b0e2f937a44b2594f88e9377af797b2895a84
eva-ustc <1326510824@qq.com>
2019-04-04 13:10:44 +0800

    Commit Update

--- src/main/java/chapter06/Sub6_1ConcurrentHashMapPutTest.java
+++ src/main/java/chapter06/Sub6_1ConcurrentHashMapPutTest.java
@@ -18,18 +18,15 @@ public class Sub6_1ConcurrentHashMapPutTest {
 //        ConcurrentHashMap hashMap = new ConcurrentHashMap();
 //        ConcurrentLinkedQueue queue;
         BlockingQueue blockingQueue;
-        final HashMap<String, String> map = new HashMap<String, String>(2);
-        Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 10000; i++) {
-                    new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            map.put(UUID.randomUUID().toString(), "");
-                        }
-                    }, "ftf" + i).start();
-                }
+        final HashMap<String, String> map = new HashMap<>(2);
+        Thread t = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        map.put(UUID.randomUUID().toString(), "");
+                    }
+                }, "ftf" + i).start();
             }
         }, "ftf");
         t.start();
